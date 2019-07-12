7c7f33377a91148c825e3028ac80a0c2134586ef
Grant Novota <47872713+Novota15@users.noreply.github.com>
2019-07-12 10:28:32 -0600

    Update server.js

--- Lab_Website_3/server.js
+++ Lab_Website_3/server.js
@@ -34,7 +34,6 @@ var db = pgp(dbConfig);
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
-app.use(flash());
 app.use(express.static(__dirname + '/'));//This line is necessary for us to use relative paths and access our resources directory
 
 
